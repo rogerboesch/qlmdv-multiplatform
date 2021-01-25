0caf6162ef9ba96ea9b5e3ce21c86dc75bcc48b7
Roger Boesch <roger@captural.com>
2021-01-25 08:47:48 +0100

    Another refactoring pass of mdv integration

--- rb_mdv.cpp
+++ rb_mdv.cpp
@@ -1,3 +1,8 @@
+/*
+    Based on Qlay Tools by Jan Venema (C) 1998
+    C++ Code by Roger Boesch
+    (Will replace qlt_mdv.c in future to use C++ 14+)
+*/
 
 #include "rb_mdv.hpp"
 
@@ -88,3 +93,20 @@ void RBMdv::SetTemporaryPath(std::string path) {
     m_temporaryPath = path;
     strcpy(temppath, path.c_str());
 }
+
+// Internal MDV access files, refactored from qlt (In progress)
+void RBMdv::PutLong(U8 *p, U32 v) {}
+void RBMdv::PutWord(U8 *p, U16 v) {}
+U32 RBMdv::GetLong(U8 *p) {}
+U16 RBMdv::GetWord(U8 *p) {}
+int RBMdv::GetXtcc(FILE *f, U32 *d) {}
+void RBMdv::OutputFile(int fnum) {}
+int RBMdv::PrintMap(int create) {}
+void RBMdv::WriteDirLen(void) {}
+void RBMdv::PutDir(void) {}
+void RBMdv::InitMdv(void) {}
+void RBMdv::PutSect(int sectlen) {}
+void RBMdv::WriteMdv(U8 *buffer, FILE *outfile) {}
+int RBMdv::GenHeader(char *ifname, U8 *head, int dz, U32 datasize) {}
+int RBMdv::File2Mdv(char *fname, char *ofname) {}
+int RBMdv::Mdv2File(char *fname, int create) {}
--- rb_mdv.hpp
+++ rb_mdv.hpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <vector>
 
+typedef unsigned long U32;
+typedef unsigned short U16;
+typedef unsigned char U8;
+
 class RBMdv {
 public:
     RBMdv();
@@ -26,7 +30,25 @@ public:
 private:
     void AddAll();
     void LoadFiles();
-    
+
+// Internal MDV access files, refactored from qlt
+private:
+    void PutLong(U8 *p, U32 v);
+    void PutWord(U8 *p, U16 v);
+    U32 GetLong(U8 *p);
+    U16 GetWord(U8 *p);
+    int GetXtcc(FILE *f, U32 *d);
+    void OutputFile(int fnum);
+    int PrintMap(int create);
+    void WriteDirLen(void);
+    void PutDir(void);
+    void InitMdv(void);
+    void PutSect(int sectlen);
+    void WriteMdv(U8 *buffer, FILE *outfile);
+    int GenHeader(char *ifname, U8 *head, int dz, U32 datasize);
+    int File2Mdv(char *fname, char *ofname);
+    int Mdv2File(char *fname, int create);
+
 private:
     bool m_loaded;
     std::string m_temporaryPath;
